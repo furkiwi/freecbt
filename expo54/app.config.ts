b95eae0a371d0daf3bb6cb18630980df4d2149b7
kiwi <furkiwi5029@gmail.com>
2026-09-23 13:08:37 +0800

    Update app.config.ts

--- expo54/app.config.ts
+++ expo54/app.config.ts
@@ -3,9 +3,9 @@ import { ConfigContext, ExpoConfig } from "expo/config";
 export default ({ config }: ConfigContext): ExpoConfig => ({
   name: "FreeCBT",
   slug: "freecbt",
-  version: "2.5.0-rc.1",
-  owner: "erosson",
-  githubUrl: "https://github.com/erosson/freecbt",
+  version: "2.5.0-rc.2",
+  owner: "furkiwi",
+  githubUrl: "https://github.com/furkiwi/freecbt",
   platforms: ["android", "ios", "web"],
   orientation: "portrait",
   icon: "./assets/ios.png",
@@ -14,7 +14,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   newArchEnabled: true,
   primaryColor: "#F8A5C2",
   ios: {
-    bundleIdentifier: "org.erosson.freecbt",
+    bundleIdentifier: "org.furkiwi.freecbt",
     supportsTablet: true,
     icon: "./assets/ios.png",
     // apple doesn't like my prerelease version tag ("2.5.0-rc.1"). remove this apple-specific override once we're done with prerelease versions.
@@ -53,11 +53,11 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   },
   updates: {
     fallbackToCacheTimeout: 0,
-    url: "https://u.expo.dev/7fbb9321-8d12-487a-a336-d12462e5f549",
+    url: "https://u.expo.dev/7845bb40-8f27-47c5-a347-ab468c88e6ab",
   },
   extra: {
     eas: {
-      projectId: "7fbb9321-8d12-487a-a336-d12462e5f549",
+      projectId: "7845bb40-8f27-47c5-a347-ab468c88e6ab",
     },
   },
   runtimeVersion: {
